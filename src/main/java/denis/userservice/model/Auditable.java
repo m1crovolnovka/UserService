1fbf6734a24bd261c12d7c0ce99c212d3ac6af90
m1crovolnovka <denisdevyaltovski@gmail.com>
2025-11-09 03:01:47 +0300

    db schema

---
+++ src/main/java/denis/userservice/model/Auditable.java
@@ -0,0 +1,24 @@
+package denis.userservice.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/resources/db/changelog/changelog-1.0-create-tables.sql
@@ -0,0 +1,29 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       name VARCHAR(100) NOT NULL,
+                       surname VARCHAR(100) NOT NULL,
+                       birth_date DATE NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       active BOOLEAN DEFAULT TRUE,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_users_email ON users(email);
+
+CREATE TABLE payment_cards (
+                               id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                               user_id UUID NOT NULL,
+                               number VARCHAR(20) NOT NULL,
+                               holder VARCHAR(150) NOT NULL,
+                               expiration_date DATE NOT NULL,
+                               active BOOLEAN DEFAULT TRUE,
+                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                               updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                               CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE INDEX idx_cards_user_id ON payment_cards(user_id);
+CREATE INDEX idx_cards_number ON payment_cards(number);
